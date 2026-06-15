19ae06e94f632640ee6846c2a2d0bb961a9492b5
JX G <fuyuuku@JXs-MacBook-Pro.local>
2026-06-15 20:20:43 +0800

    auto: update at 2026-06-15 20:20:43

--- frontend/src/stores/settingsStore.ts
+++ frontend/src/stores/settingsStore.ts
@@ -9,10 +9,12 @@ interface SettingsState {
   ttsEnabled: boolean
   stylePreset: StylePreset
   fontSize: FontSize
+  compactMode: boolean
   setLive2dEnabled: (value: boolean) => void
   setTtsEnabled: (value: boolean) => void
   setStylePreset: (value: StylePreset) => void
   setFontSize: (value: FontSize) => void
+  setCompactMode: (value: boolean) => void
 }
 
 export const useSettingsStore = create<SettingsState>()(
@@ -22,10 +24,12 @@ export const useSettingsStore = create<SettingsState>()(
       ttsEnabled: false,
       stylePreset: 'cel',
       fontSize: 'default',
+      compactMode: false,
       setLive2dEnabled: (value) => set({ live2dEnabled: value }),
       setTtsEnabled: (value) => set({ ttsEnabled: value }),
       setStylePreset: (value) => set({ stylePreset: value }),
       setFontSize: (value) => set({ fontSize: value }),
+      setCompactMode: (value) => set({ compactMode: value }),
     }),
     {
       name: 'elaw-settings',
